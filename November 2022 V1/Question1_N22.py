56b8b59966fec44086005c4b189470c399956234
Ramy Hazem AWAD <rawad@students.cesegypt.com>
2024-02-19 08:32:05 +0200

    completed q1

--- November 2022 V1/Question1_N22.py
+++ November 2022 V1/Question1_N22.py
@@ -13,5 +13,37 @@ def ReadFile():
         return "File not found"
 
 
+def FindValues():
+    count = 0
+    while True:
+        num_to_search = input("Please enter a number to search in the array: ")
+        if num_to_search.isdigit():
+            num_to_search = int(num_to_search)
+            break
+        else:
+            print("Value inputted is not a valid number, try again. ")
+
+    if 1 <= num_to_search <= 100:
+        for num in DataArray:
+            if num_to_search == num:
+                count += 1
+
+    return count, num_to_search
+
+
 ReadFile()
-print(DataArray)
+no_of_appearances, value = FindValues()
+print(f"The value {value} was found {no_of_appearances} times! ")
+
+
+def BubbleSort():
+    for i in range(len(DataArray)):
+        for y in range(len(DataArray) - 1):
+            if DataArray[y] > DataArray[y + 1]:
+                DataArray[y + 1], DataArray[y] = DataArray[y], DataArray[y + 1]
+
+    return DataArray
+
+
+print(BubbleSort())
+
