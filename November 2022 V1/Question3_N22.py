1971dfffd17fdf67cc4647b84b92c56587a3d482
Ramy Awad <ramyaqt20@gmail.com>
2024-03-03 18:59:06 +0200

    q3 complete (binary tree with search and post order traversal

--- November 2022 V1/Question3_N22.py
+++ November 2022 V1/Question3_N22.py
@@ -1,7 +1,10 @@
 array_nodes = [[-1, -1, -1] for i in range(20)]
 
+array_nodes = [[1, 20, 5], [2, 15, -1], [-1, 3, 3], [-1, 9, 4], [-1, 10, -1], [-1, 58, -1], [-1, -1, -1]]
+
 free_node = 6
 root_pointer = 0
+
 
 def SearchValue(root, value):
     if root == -1:
@@ -12,8 +15,24 @@ def SearchValue(root, value):
     elif array_nodes[root][1] == -1:
         return -1
 
-    if array_nodes[root][1] < value:
+    if array_nodes[root][1] > value:
         return SearchValue(array_nodes[root][0], value)
 
-    if array_nodes[root][1] > value:
+    if array_nodes[root][1] < value:
         return SearchValue(array_nodes[root][2], value)
+
+
+def post_order_traversal(root):
+    if array_nodes[root][0] != -1:
+        post_order_traversal(array_nodes[root][0])
+    if array_nodes[root][2] != -1:
+        post_order_traversal(array_nodes[root][2])
+    print(array_nodes[root][1])
+
+
+answer = SearchValue(root_pointer, 15)
+if answer == -1:
+    print("Value not found in the tree")
+else:
+    print(f"Value was found at index: {answer}")
+post_order_traversal(root_pointer)
